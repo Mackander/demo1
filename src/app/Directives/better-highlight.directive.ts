036e575955f1f5292210d048fbe6cc94af2bea91
Mackander Singh <m.singh@iaea.org>
2017-11-01 10:38:08 +0100

    Added structural and attribute directive

---
+++ src/app/Directives/better-highlight.directive.ts
@@ -0,0 +1,32 @@
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+
+  @Input() highlightColor: string = 'cyan';
+  @Input() defaultColor: string = 'transparent';
+
+  // We can either use renderer to style the element or
+  // we can use @HostBinding in which we pass string to which host property we want to bind
+  // so here we binding it to style.backgroundColor property
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'cyan');
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'cyan');
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.defaultColor;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AssignmentComponent } from '../app/assignment/assignment.component'
@@ -8,16 +8,20 @@ import { Assignment3Component } from '../app/assignment3/assignment3.component';
 import { GameControlComponent } from './assignment4/game-control/game-control.component';
 import { OddComponent } from './assignment4/odd/odd.component';
 import { EvenComponent } from './assignment4/even/even.component';
+import { BetterHighlightDirective } from './Directives/better-highlight.directive';
+import { UnlessDirective } from './Directives/unless.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    //AssignmentComponent,
-    //Assignment3Component,
+    // AssignmentComponent,
+    // Assignment3Component,
     GameControlComponent,
     OddComponent,
-    EvenComponent
+    EvenComponent,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
